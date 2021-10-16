cf2608870b9f12f8033bd0b282f0c17853f734c4
Kelvin Dealca <dealca@fsi.io>
2021-10-16 12:43:41 -0700

    Fix sticking when colliding with the border of the level

--- player/Player.gd
+++ player/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var ACCELERATION: int = 300
+export var ACCELERATION: int = 350
 export var MAX_SPEED: int = 500
 export var FRICTION: int = 200
 
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
 	if input_vector != Vector2.ZERO:
 #		animationTree.set("parameters/Idle/blend_position", input_vector)
 #		animationTree.set("parameters/Run/blend_position", input_vector)
@@ -25,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	else:
 #		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	move_and_slide(velocity, Vector2.UP)
-	
+
+	velocity = move_and_slide(velocity, Vector2.UP) # Vector2.UP is Vector2(0, -1), pointing up
+
 	# TODO: If a collision happened then stop it right away so we can change directions faster

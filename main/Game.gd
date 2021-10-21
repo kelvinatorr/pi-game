890d010eb3067bdf9663dfcc78b7e29492a40976
Kelvin Dealca <dealca@fsi.io>
2021-10-21 15:32:30 -0700

    Add Tween to the energy bar

--- main/Game.gd
+++ main/Game.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+var turtle_health: int = 1000
+var energy_per_second: int = 1
 
 func _ready() -> void:
 	$EnergyTimer.start()
 
 func _on_EnergyTimer_timeout() -> void:
-	print('Energy Timer tick')
-	$UI.reduce_energy(0.1)
+	turtle_health -= energy_per_second
+	if turtle_health < 0:
+		print('Game Over')
+	else:
+		$UI.update_energy(turtle_health)
--- ui/UI.gd
+++ ui/UI.gd
@@ -1,6 +1,18 @@
 extends CanvasLayer
 
 onready var energy_bar: TextureProgress = $HUD/InfoBar/HBoxContainer/TextureProgress
+onready var energy_bar_tween: Tween = $HUD/InfoBar/HBoxContainer/TextureProgress/Tween
 
-func reduce_energy(val: float) -> void:
-	energy_bar.value -= val
+func update_energy(val: int) -> void:
+	energy_bar.value = val
+	energy_bar_tween.interpolate_property(energy_bar, 'value',
+		energy_bar.value, val, 1,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	energy_bar_tween.start()
+
+	if energy_bar.value >= 600:
+		energy_bar.set_tint_progress('12b624') # Green
+	elif energy_bar.value <= 600 and energy_bar.value >= 250:
+		energy_bar.set_tint_progress('e1be32') # Orange
+	else:
+		energy_bar.set_tint_progress('e11e1e') # Red
